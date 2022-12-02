c8854b97347792cf515582f6fc52da1c6b05ea6b
Jochem Arends <77842807+jochemarends@users.noreply.github.com>
2022-12-02 15:12:46 +0100

    Update day02.cpp

--- 2022/day02.cpp
+++ 2022/day02.cpp
@@ -12,6 +12,7 @@ enum Choice {
     scissors
 };
 
+/* does a win from b? */
 bool operator>(Choice a, Choice b) {
     switch (a) {
     case rock:
@@ -23,6 +24,7 @@ bool operator>(Choice a, Choice b) {
     }
 }
 
+/* does a lose from b? */
 bool operator<(Choice a, Choice b) {
     switch (a) {
     case rock:
@@ -46,19 +48,44 @@ int main() try {
     std::vector<char> vec(std::istream_iterator<char>{ifs},
                           std::istream_iterator<char>{});
 
-    int score = 0;
+    int score1 = 0;
+    int score2 = 0;
     for (std::size_t i = 1; i < vec.size(); i += 2) {
-        Choice a = map.at(vec[i]);
-        Choice b = map.at(vec[i - 1]);
+        Choice a = map.at(vec[i - 1]);
+        Choice b = map.at(vec[i]);
 
-        score += static_cast<int>(a); // your choice
+        score1 += static_cast<int>(b); // your choice
 
-        if (a > b)		score += 6;	// win
-        else if (a < b) score += 0; // loss
-        else			score += 3; // draw
+        if (a < b)		score1 += 6; // win
+        else if (a > b) score1 += 0; // loss
+        else			score1 += 3; // draw
+
+        /* part 2 */
+        Choice c;
+        switch (vec[i]) {
+        case 'X':               // lose
+            if (a > rock)       c = rock;
+            else if (a > paper) c = paper;
+            else                c = scissors;
+            break;
+        case 'Y':               // draw
+            c = a;
+            score2 += 3;
+            break;
+        case 'Z':               // win
+            if (a < rock)       c = rock; 
+            else if (a < paper) c = paper;
+            else                c = scissors;
+            score2 += 6;
+            break;
+        default:
+            throw std::runtime_error{ "invalid input" };
+        }
+        score2 += static_cast<int>(c);
     }
 
-    std::cout << "part 1: " << score << '\n';
+    std::cout << "part 1: " << score1 << '\n';
+    std::cout << "part 2: " << score2 << '\n';
 }
 catch (std::exception& e) {
     std::cerr << e.what() << '\n';
