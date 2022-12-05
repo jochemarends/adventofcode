e62650b46dbfcdeb9b8d7f28c537ef5af26c3c8b
Jochem Arends <77842807+jochemarends@users.noreply.github.com>
2022-12-05 13:14:43 +0100

    Update day05(not_complete).cpp

--- 2022/day05(not_complete).cpp
+++ 2022/day05(not_complete).cpp
@@ -5,56 +5,72 @@
 #include <stack>
 #include <algorithm>
 #include <numeric>
+#include <ranges>
 
 using Column = std::stack<char>;
 
 int main() try {
-	std::ifstream ifs{ "input.txt" };
-	if (!ifs) throw std::runtime_error{ "failed to open input file" };
+    std::ifstream ifs{ "input.txt" };
+    if (!ifs) throw std::runtime_error{ "failed to open input file" };
 
-	std::vector<std::string> lines;
-	for (std::string line; std::getline(ifs, line);) {
-		if (line.empty()) break;
-		lines.push_back(line);
-	}
+    /* read until a blank line */
+    std::vector<std::string> lines;
+    for (std::string line; std::getline(ifs, line) && !line.empty();)
+        lines.push_back(line);
 
-	std::vector<Column> v{ (lines.back().size() + 1) / 4 };
+    std::size_t columns = 0;
+    for (char ch : lines.back()) {
+        if (isdigit(ch)) ++columns;
+    }
 
-	std::for_each(lines.rbegin() + 1, lines.rend(),
-		[&v](const std::string& line) {
-			for (size_t i = 0; i < v.size(); ++i) {
-				char ch = line[1 + 4 * i];
-				if (!std::isspace(ch)) {
-					v[i].push(ch);
-				}
-			}
-		});
+    std::vector<std::stack<char>> v{ columns };
 
-	while (true) {
-		std::string s;
-		int count, from, to;
-		ifs >> s >> count >> s >> from >> s >> to;
-		if (!ifs) break;
-		
-		std::vector<char> vec;
-		for (std::size_t i = 0; i < count; ++i) {
-			vec.push_back(v[from - 1].top());
-			v[from - 1].pop();
-		}
+    /* push the data onto the stack structures */
+    for (const std::string& line : std::views::reverse(lines)) {
+        for (std::size_t i = 0; i < v.size(); ++i) {
+            char ch = line[1 + 4 * i];
+            if (std::isspace(ch)) continue;
+            v[i].push(ch);
+        }
+    }
 
+    auto part1{ v }, part2{ v };
 
-		std::reverse(vec.begin(), vec.end());
-		for (auto ch : vec) {
-			v[to - 1].push(ch);
-		}
-	}
+    /* read the instructions */
+    while (!ifs.eof()) {
+        std::string s;
+        std::size_t count, src_index, dst_index;
+        ifs >> s >> count >> s >> src_index 
+            >> s >> dst_index;
 
-	for (auto col : v) {
-		if (col.size() == 0) continue;
-		std::cout << col.top();
-	}
+        /* convert from 1 to 0 based index */
+        const auto src_iter1 = part1.begin() + src_index - 1;
+        const auto dst_iter1 = part1.begin() + dst_index - 1;
+
+        const auto src_iter2 = part2.begin() + src_index - 1;
+        const auto dst_iter2 = part2.begin() + dst_index - 1;
+
+        std::vector<char> temp;
+        for (std::size_t i = 0; i < count; ++i) {
+            /* part 1 */
+            dst_iter1->push(src_iter1->top());
+            src_iter1->pop();
+
+            /* part 2*/
+            temp.push_back(src_iter2->top());
+            src_iter2->pop();
+        }
+
+        /* push the elements in reversed order */
+        std::for_each(temp.rbegin(), temp.rend(), [&dst_iter2](char ch) { dst_iter2->push(ch); });
+    }
+
+    std::cout << "part 1: ";
+    std::ranges::for_each(part1, [](auto& stack) { std::cout << stack.top(); });
+    std::cout << "\npart 2: ";
+    std::ranges::for_each(part2, [](auto& stack) { std::cout << stack.top(); });
 }
 catch (std::exception& e) {
-	std::cerr << e.what() << '\n';
-	return 0;
+    std::cerr << e.what() << '\n';
+    return 0;
 }
