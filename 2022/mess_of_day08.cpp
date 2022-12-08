7e6c63e6c4f30e071a2f18f205ca9d76369a2a00
Jochem Arends <77842807+jochemarends@users.noreply.github.com>
2022-12-08 13:51:00 +0100

    Update mess_of_day08.cpp

--- 2022/mess_of_day08.cpp
+++ 2022/mess_of_day08.cpp
@@ -19,7 +19,8 @@ int main() try {
     }
 
     std::size_t part1 = 0;
-    std::vector<std::size_t> part2;
+    int part2 = 0;
+    std::vector<int> scores;
     for (auto it1 = vec.begin(); it1 != vec.end(); ++it1) {
         const auto& row = *it1;
 
@@ -67,41 +68,27 @@ int main() try {
             /* extract trees below */
             std::vector<int> below;
             std::transform(std::next(it1), vec.end(), std::back_inserter(below), [index](const auto& row) { return row[index]; });
-            //std::ranges::reverse(below);
             if (std::ranges::max(below) < height) {
                 is_visible = true;
             }
 
             if (is_visible) ++part1;
 
-            //std::ranges::for_each(below, [](auto elem) { std::cout << elem << '\n'; });
-            //std::cout << std::distance(below.begin(), std::ranges::adjacent_find(below, std::greater_equal())) + 1 << '\n';
-
-            /* calculates the viewing dist in a given direction */
-            auto view_dist = [&](const std::vector<int>& vec) -> std::size_t {
-                if (vec.front() >= height) return 1;
-                std::size_t n = 1;
-                for (std::size_t i = 1; i < vec.size(); ++i, ++n) {
-                    if (vec[i - 1] > vec[i]) break;
-                }
-                return n;
-                return std::distance(vec.begin(), std::ranges::adjacent_find(vec, std::greater_equal())) + 1;
-            };
-
             /* part 2 */
-            std::vector<std::size_t> dists(4);
-            dists[0] = view_dist(above);
-            dists[1] = view_dist(left);
-            dists[2] = view_dist(right);
-            dists[3] = view_dist(below);
-
-            part2.push_back(std::accumulate(dists.begin(), dists.end(), 1, std::multiplies()));
-            std::ranges::for_each(dists, [](auto elem) { std::cout << elem << '\n'; });
-            std::cout << '\n';
+            std::vector<int> distances;
+            for (const auto& vec : { above, left, right, below }) {
+                distances.push_back({});
+                for (int i : vec) {
+                    ++distances.back();
+                    if (height <= i) break;
+                }
+            }
+            scores.push_back(std::accumulate(distances.begin(), distances.end(), 1, std::multiplies()));
         }
+        part2 = std::ranges::max(scores);
     }
     std::cout << "part 1: " << part1 << '\n';
-    std::cout << "part 2: " << std::ranges::max(part2) << '\n';
+    std::cout << "part 2: " << part2 << '\n';
 }
 catch (std::exception& e) {
     std::cerr << e.what() << '\n';
