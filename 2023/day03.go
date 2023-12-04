f5e77e1fd6c05a01c2b903b2f088ebdbec6353c5
Jochem Arends <joch.arends@gmail.com>
2023-12-04 15:23:37 +0100

    Finished the second part of day 3.

--- 2023/day03.go
+++ 2023/day03.go
@@ -9,51 +9,28 @@ import (
     "strconv"
 )
 
-func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
-}
-
 func min(a, b int) int {
     if a < b {
-        return a
+        return a;
     }
-    return b
+    return b;
 }
 
-func partOne(schematic []string) int {
-    pattern := "[0-9]+"
-    regex := regexp.MustCompile(pattern)
-    sum := 0
-
-    for y, line := range schematic {
-        getAdjacentBytes := func(x, y, count int) (bytes []byte) {
-            for i := max(x - 1, 0); i < min(x + count + 1, len(line)); i++ {
-                for j := max(y - 1, 0); j < min(y + 2, len(schematic)); j++ {
-                    if j != y || i < x || i >= x + count {
-                        bytes = append(bytes, schematic[j][i])
-                    }
-                }
-            }
-            return
-        }
-
-        for _, indices := range regex.FindAllStringIndex(line, -1) {
-            x, count := indices[0], indices[1] - indices[0]
-            for _, b := range getAdjacentBytes(x, y, count) {
-                if !unicode.IsDigit(rune(b)) && b != '.' {
-                    str := line[x: x + count]
-                    num, _ := strconv.Atoi(str)
-                    sum += num
-                    break
-                }
-            }
-        }
+func max(a, b int) int {
+    if a > b {
+        return a;
     }
+    return b;
+}
 
-    return sum
+type Point struct {
+    X int
+    Y int
+}
+
+type Region struct {
+    Point
+    Count int
 }
 
 func main() {
@@ -71,6 +48,56 @@ func main() {
         schematic = append(schematic, scanner.Text())
     }
 
-    fmt.Println("Part01:", partOne(schematic))
+    text := func(r Region) string {
+        return schematic[r.Y][r.X:r.X + r.Count]
+    }
+
+    number := func(r Region) int {
+        num, _ := strconv.Atoi(text(r))
+        return num
+    }
+
+    pattern := `\d+`
+    regex := regexp.MustCompile(pattern)
+
+    var part01 int
+    var partNumbers []Region
+    asterisks := make(map[Point][]Region)
+    for y, line := range schematic {
+        adjacentPoints := func(r Region) (points []Point) {
+            for x := max(r.X - 1, 0); x < min(r.X + r.Count + 1, len(line)); x++ {
+                for y := max(r.Y - 1, 0); y < min(r.Y + 2, len(schematic)); y++ {
+                    points = append(points, Point{x, y})
+                }
+            }
+            return
+        }
+
+        for _, indices := range regex.FindAllStringIndex(line, -1) {
+            region := Region{Point{indices[0], y}, indices[1] - indices[0]}
+            isPartNumber := false
+            for _, p := range adjacentPoints(region) {
+                r := rune(schematic[p.Y][p.X])
+                if r == '*' {
+                    asterisks[p] = append(asterisks[p], region)
+                }
+                if !isPartNumber && !unicode.IsDigit(r) && r != '.' {
+                    partNumbers = append(partNumbers, region)
+                    part01 += number(region)
+                    isPartNumber = true
+                }
+            }
+        }
+    }
+
+    var part02 int
+    for _, regions := range asterisks {
+        if len(regions) == 2 {
+            part02 += number(regions[0]) * number(regions[1])
+        }
+    }
+
+    fmt.Println("Part01:", part01)
+    fmt.Println("Part02:", part02)
 }
 
