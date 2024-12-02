8a1ee22d3818a372728478519cd5e6c3edcb58c9
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2024-12-02 17:01:00 +0100

    add solution in C#

---
+++ 2024/day02.cs
@@ -0,0 +1,39 @@
+namespace AdventOfCode
+{
+    class Solution
+    {
+        static bool IsSuperSafe(IEnumerable<int> report)
+        {
+            var gaps = report.Skip(1).Zip(report, (a, b) => a - b);
+            var signs = gaps.Select(Math.Sign);
+            return gaps.Select(Math.Abs).All(n => n >= 1 && n <= 3) 
+                && signs.Skip(1).All(n => n == signs.First());
+        }
+
+        static bool IsSafe(IEnumerable<int> report)
+            => report.Select((_, index) => report.Where((_, idx) => index != idx))
+                .Any(IsSuperSafe);
+
+        static int Part1(IEnumerable<IEnumerable<int>> input)
+            => input.Where(IsSuperSafe).Count();
+
+        static int Part2(IEnumerable<IList<int>> input)
+            => input.Where(IsSafe).Count();
+
+        static IEnumerable<IList<int>> Parse(StreamReader stream)
+        {
+            string? line;
+            while ((line = stream.ReadLine()) != null) 
+            {
+                yield return line.Split(' ').Select(Int32.Parse).ToList();
+            }
+        }
+
+        static void Main()
+        {
+            var input = Parse(new StreamReader("./input.txt")).ToList();
+            Console.WriteLine($"part 1: {Part1(input)}");
+            Console.WriteLine($"part 2: {Part2(input)}");
+        }
+    }
+}
