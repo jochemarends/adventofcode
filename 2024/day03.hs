1e441264763a573f6a1672111e69fc38ad07a4f3
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2024-12-03 10:19:55 +0100

    working on day three

--- 2024/day03.hs
+++ 2024/day03.hs
@@ -9,27 +9,34 @@ data Token
     | RParen
     deriving (Show, Eq)
 
-data Expr = Times Int Int deriving (Show, Eq)
+data Expr 
+    = Term Expr Expr
+    | Primary Token
+    deriving (Show, Eq)
 
 tokenize :: String -> [Token]
 tokenize [] = []
-tokenize s@(x:xs)
+tokenize all@(x:xs)
     | x == ',' = Comma : tokenize xs
     | x == '(' = LParen : tokenize xs
     | x == ')' = RParen : tokenize xs
     | isDigit x = let (digits, other) = span isDigit (x:xs)
         in Number digits : tokenize other
-    | isPrefixOf "mul" s = Mul : tokenize (drop 3 s)
+    | isPrefixOf "mul" all = Mul : tokenize (drop 3 all)
     | otherwise = tokenize xs
 
 parse :: [Token] -> [Expr]
 parse [] = []
-parse (Mul:Number a:Number b:_) = [Times (read a) (read b)]
+parse [Number x] = [Primary (read x)]
+parse (LParen:xs) = 
+    let (before, after) = break (==RParen) xs
+    in case after of
+        (_:toks) -> parse toks
+        [] -> error "no right paren was found"
+parse (Mul:LParen:Number a:Comma:Number b:RParen:rest) = (parse [a]) * (parse b) + parse rest
 parse (_:xs) = parse xs
 
 main :: IO ()
 main = do
     input <- readFile "./input.txt"
     print $ parse $ tokenize input
-    print input
-    -- printf "part 1: %d\n" (part1 input)
