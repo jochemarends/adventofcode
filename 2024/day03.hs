a7b9c55c92941ff724a4a9cfc0481fab619af503
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2024-12-03 14:28:45 +0100

    part one

--- 2024/day03.hs
+++ 2024/day03.hs
@@ -1,42 +1,43 @@
 import Data.Char
-import Data.List (isPrefixOf)
+import Data.List (stripPrefix)
+import Text.Printf
 
 data Token 
-    = Mul
+    = Mul | Pause | Resume
     | Number String 
     | Comma 
-    | LParen 
-    | RParen
+    | LParen | RParen
+    | Corrupt
     deriving (Show, Eq)
 
-data Expr 
-    = Term Expr Expr
-    | Primary Token
-    deriving (Show, Eq)
+data Expr = Term Int Int deriving (Show, Eq)
 
 tokenize :: String -> [Token]
 tokenize [] = []
 tokenize all@(x:xs)
-    | x == ',' = Comma : tokenize xs
+    | x == ',' = Comma  : tokenize xs
     | x == '(' = LParen : tokenize xs
     | x == ')' = RParen : tokenize xs
-    | isDigit x = let (digits, other) = span isDigit (x:xs)
-        in Number digits : tokenize other
-    | isPrefixOf "mul" all = Mul : tokenize (drop 3 all)
-    | otherwise = tokenize xs
+    | isDigit x = 
+        let (digits, other) = span isDigit (x:xs)
+        in if length digits `elem` [1..3]
+        then Number digits : tokenize other
+        else tokenize other
+    | Just rest <- stripPrefix "mul"   all = Mul    : tokenize rest
+    | Just rest <- stripPrefix "do"    all = Resume : tokenize rest
+    | Just rest <- stripPrefix "don't" all = Pause  : tokenize rest
+    | otherwise = Corrupt : tokenize xs
 
 parse :: [Token] -> [Expr]
 parse [] = []
-parse [Number x] = [Primary (read x)]
-parse (LParen:xs) = 
-    let (before, after) = break (==RParen) xs
-    in case after of
-        (_:toks) -> parse toks
-        [] -> error "no right paren was found"
-parse (Mul:LParen:Number a:Comma:Number b:RParen:rest) = (parse [a]) * (parse b) + parse rest
-parse (_:xs) = parse xs
+parse (Mul:LParen:Number a:Comma:Number b:RParen:rest) = Term (read a) (read b) : parse rest
+parse (x:xs) = parse xs
+
+eval :: [Expr] -> [Int]
+eval [] = []
+eval (Term a b:rest) = a * b : eval rest
 
 main :: IO ()
 main = do
     input <- readFile "./input.txt"
-    print $ parse $ tokenize input
+    printf "part 1 %d\n" $ sum $ eval $ parse $ tokenize input
