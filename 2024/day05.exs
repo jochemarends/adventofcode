4bb242d1c273c1d815c1d8c7eee6bbf6ad5325f7
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2024-12-06 00:55:31 +0100

    update day five

--- 2024/day05.exs
+++ 2024/day05.exs
@@ -19,26 +19,25 @@ defmodule Day5 do
 
   defp is_sorted?(update, rules), do: Enum.all?(rules, &is_sorted?(update, &1))
 
-  defp middle(list), do: Enum.at(list, length(list) |> div(2))
-
-  defp sum_middles(updates), do: Enum.map(updates, &middle/1) |> Enum.sum()
-
-  def part1([rules, updates]) do
-    updates
-    |> Enum.filter(&is_sorted?(&1, rules))
-    |> sum_middles()
-  end
-
   defp sort(update, rules), do: Enum.sort(update, &is_sorted?([&1, &2], rules))
 
-  def part2([rules, updates]) do
-    updates
-    |> Enum.reject(&is_sorted?(&1, rules))
-    |> Enum.map(&sort(&1, rules))
-    |> sum_middles()
+  defp middle(list), do: Enum.at(list, length(list) |> div(2))
+
+  def solve([rules, updates], part) do
+    case part do
+      :part1 -> 
+        updates
+        |> Enum.filter(&is_sorted?(&1, rules)) 
+      :part2 -> 
+        updates
+        |> Enum.reject(&is_sorted?(&1, rules))
+        |> Enum.map(&sort(&1, rules))
+    end
+    |> Enum.map(&middle/1) 
+    |> Enum.sum()
   end
 end
 
 input = Day5.parse(File.read("input.txt") |> elem(1))
-IO.puts("part 1: #{Day5.part1(input)}")
-IO.puts("part 2: #{Day5.part2(input)}")
+IO.puts("part 1: #{Day5.solve(input, :part1)}")
+IO.puts("part 2: #{Day5.solve(input, :part2)}")
