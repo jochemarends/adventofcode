f4a355947063c180799407c433cd374e29e0c4a7
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2024-12-05 23:58:29 +0100

    update day five

--- 2024/day05.exs
+++ 2024/day05.exs
@@ -1,20 +1,11 @@
 defmodule Day5 do
-  def parse(line) when is_bitstring(line) do
-    line
-    |> String.split(~r/[|,]/, trim: true)
-    |> Enum.map(&String.to_integer/1)
-  end
+  defp split(string, :parts), do: String.split(string, "\n\n")
+  defp split(string, :lines), do: String.split(string, "\n", trim: true)
+  defp split(string, :elems), do: String.split(string, ["|", ","], trim: true) 
 
-  def parse(lines) when is_list(lines), do: Enum.map(lines, &parse/1)
-
-  def parse(enum) do
-    enum
-    |> Stream.map(&String.trim/1)
-    |> Stream.chunk_by(&(&1 == ""))
-    |> Enum.filter(&(&1 != [""]))
-    |> Enum.map(&parse/1)
-    |> List.to_tuple()
-  end
+  def parse(string), do: split(string, :parts) |> Enum.map(&parse(&1, :lines))
+  def parse(part, :lines), do: split(part, :lines) |> Enum.map(&parse(&1, :elems))
+  def parse(line, :elems), do: split(line, :elems) |> Enum.map(&String.to_integer/1)
 
   defp is_sorted?(update, [_, _] = rule) do
     case Enum.map(rule, fn n -> Enum.find_index(update, &(&1==n)) end) do
@@ -30,7 +21,7 @@ defmodule Day5 do
 
   defp sum_midpoints(updates), do: Enum.map(updates, &middle/1) |> Enum.sum()
 
-  def part1({ rules, updates }) do
+  def part1([rules, updates]) do
     updates
     |> Enum.filter(&is_sorted?(&1, rules))
     |> sum_midpoints()
@@ -38,7 +29,7 @@ defmodule Day5 do
 
   defp sort(update, rules), do: Enum.sort(update, &is_sorted?([&1, &2], rules))
 
-  def part2({ rules, updates }) do
+  def part2([rules, updates]) do
     updates
     |> Enum.reject(&is_sorted?(&1, rules))
     |> Enum.map(&sort(&1, rules))
@@ -46,5 +37,6 @@ defmodule Day5 do
   end
 end
 
-input = Day5.parse(File.stream!("input.txt"))
-IO.puts("part 1: #{Day5.part1(input)}\npart 2: #{Day5.part2(input)}")
+input = Day5.parse(File.read("input.txt") |> elem(1))
+IO.puts("part 1: #{Day5.part1(input)}")
+IO.puts("part 2: #{Day5.part2(input)}")
