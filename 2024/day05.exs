3486f149c270ed302b280ad137ef1dfbc943e0c6
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2024-12-05 14:31:57 +0100

    day five

--- 2024/day05.exs
+++ 2024/day05.exs
@@ -1,12 +1,9 @@
 defmodule Day5 do
   def parse(line) when is_bitstring(line) do
     line
-    String.length
     |> String.split(~r{[|,]}, trim: true)
-    |> Enum.map(&Float.parse/1)
-    |> Enum.map(&IO.inspect/1)
+    |> Enum.map(&Integer.parse/1)
     |> Enum.map(&elem(&1, 0)) 
-    |> IO.inspect()
   end
 
   def parse(lines) when is_list(lines) do
@@ -18,25 +15,58 @@ defmodule Day5 do
     |> Stream.map(&String.trim/1)
     |> Stream.chunk_by(&(&1 == ""))
     |> Enum.filter(&(&1 != [""]))
-    |> IO.inspect()
     |> Enum.map(&parse/1)
+    |> List.to_tuple()
   end
 
-  defp is_ordered?([[a, b]], update) do
-    Enum.find(b, &(&1==a)) >= Enum.find(a, &(&1==b))
+  defp is_ordered?([_, _] = rule, update) do
+    indices = Enum.map(rule, fn n -> Enum.find_index(update, &(&1==n)) end)
+    case indices do
+      [nil, _] -> true
+      [_, nil] -> true
+      [a, b] -> a < b
+    end
   end
 
-  defp is_ordered?([a, b], update) do
-    is_ordered?(a, update) and is_ordered?(b, update)
+  defp is_ordered?(rules, update) do
+    Enum.all?(rules, &is_ordered?(&1, update))
+  end
+
+  defp middle(list), do: Enum.at(list, length(list) |> div(2))
+
+  def sorter(a, b, rules) do
+    rule = Enum.find(rules, fn rule -> 
+      case rule do
+        [^a, ^b] -> true
+        [^b, ^a] -> true
+        _ -> false
+      end
+    end)
+
+    case rule do
+      [^a, ^b] -> a < b
+      [^b, ^a] -> b > a
+      _ -> false
+    end
+  end
+
+  def sort(rules, update) do
+    Enum.sort(update, &sorter(&1, &2, rules))
   end
 
   def part1({ rules, updates }) do
     updates
-    |> Enum.count(&is_ordered?(rules, &1))
+    |> Enum.filter(&is_ordered?(rules, &1))
+    |> Enum.map(&middle/1)
+    |> Enum.sum()
+  end
+
+  def part2({ rules, updates }) do
+    sort(rules, Enum.at(updates, 0))
   end
 end
 
-foo = File.stream!("input.txt")
+input = Day5.parse(File.stream!("input.txt"))
+IO.puts("part 1: #{Day5.part1(input)}")
+Day5.part2(input) |> Enum.take(3) |> IO.puts()
 
-Day5.parse(foo)
-|> IO.puts()
