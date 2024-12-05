9b3cc510b6bd4938a06ade67aed10ac9debd8726
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2024-12-05 20:30:45 +0100

    update day five

--- 2024/day05.exs
+++ 2024/day05.exs
@@ -1,14 +1,11 @@
 defmodule Day5 do
   def parse(line) when is_bitstring(line) do
     line
-    |> String.split(~r{[|,]}, trim: true)
-    |> Enum.map(&Integer.parse/1)
-    |> Enum.map(&elem(&1, 0)) 
+    |> String.split(~r/[|,]/, trim: true)
+    |> Enum.map(&String.to_integer/1)
   end
 
-  def parse(lines) when is_list(lines) do
-    Enum.map(lines, &parse/1)
-  end
+  def parse(lines) when is_list(lines), do: Enum.map(lines, &parse/1)
 
   def parse(enum) do
     enum
@@ -19,58 +16,35 @@ defmodule Day5 do
     |> List.to_tuple()
   end
 
-  defp is_ordered?([_, _] = rule, update) do
-    indices = Enum.map(rule, fn n -> Enum.find_index(update, &(&1==n)) end)
-    case indices do
+  defp is_sorted?(update, [_, _] = rule) do
+    case Enum.map(rule, fn n -> Enum.find_index(update, &(&1==n)) end) do
       [nil, _] -> true
       [_, nil] -> true
       [a, b] -> a < b
     end
   end
 
-  defp is_ordered?(rules, update) do
-    Enum.all?(rules, &is_ordered?(&1, update))
-  end
+  defp is_sorted?(update, rules), do: Enum.all?(rules, &is_sorted?(update, &1))
 
   defp middle(list), do: Enum.at(list, length(list) |> div(2))
 
-  def sorter(a, b, rules) do
-    rule = Enum.find(rules, fn rule -> 
-      case rule do
-        [^a, ^b] -> true
-        [^b, ^a] -> true
-        _ -> false
-      end
-    end)
-
-    case rule do
-      [^a, ^b] -> a <= b
-      [^b, ^a] -> b <= a
-      _ -> true
-    end
-  end
-
-  def sort(rules, update) do
-    Enum.sort(update, &sorter(&1, &2, rules))
-  end
+  defp sum_midpoints(updates), do: Enum.map(updates, &middle/1) |> Enum.sum()
 
   def part1({ rules, updates }) do
     updates
-    |> Enum.filter(&is_ordered?(rules, &1))
-    |> Enum.map(&middle/1)
-    |> Enum.sum()
+    |> Enum.filter(&is_sorted?(&1, rules))
+    |> sum_midpoints()
   end
+
+  defp sort(update, rules), do: Enum.sort(update, &is_sorted?([&1, &2], rules))
 
   def part2({ rules, updates }) do
     updates
-    |> Enum.reject(&is_ordered?(rules, &1))
-    |> Enum.map(&sort(rules, &1))
-    |> Enum.map(&middle/1)
-    |> Enum.sum()
+    |> Enum.reject(&is_sorted?(&1, rules))
+    |> Enum.map(&sort(&1, rules))
+    |> sum_midpoints()
   end
 end
 
 input = Day5.parse(File.stream!("input.txt"))
-IO.puts("part 1: #{Day5.part1(input)}")
-IO.puts("part 2: #{Day5.part2(input)}")
-
+IO.puts("part 1: #{Day5.part1(input)}\npart 2: #{Day5.part2(input)}")
