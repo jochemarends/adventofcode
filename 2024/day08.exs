b09a3d835c71074df10472ca29b3affd38cbc94b
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2024-12-08 12:52:24 +0100

    day eight part two

--- 2024/day08.exs
+++ 2024/day08.exs
@@ -5,16 +5,11 @@ defmodule Day8 do
     |> Enum.map(&String.graphemes/1)
   end
 
-  def rows(map), do: length(map)
-  def cols(map), do: length(hd(map))
-  def at(map, {row, col}) when row < 0 or col < 0, do: nil
+  def at(_, {row, col}) when row < 0 or col < 0, do: nil
   def at(map, {row, col}), do: Enum.at(Enum.at(map, row, []), col)
-  def distance({x1, y1}, {x2, y2}), do: {abs(x2 - x1), abs(y2 - y1)}
 
   def indices(map) do
-    for row <- 0..(rows(map) - 1), col <- 0..(cols(map) - 1) do
-      {row, col}
-    end
+    for row <- 0..(length(map) - 1), col <- 0..(length(hd(map)) - 1), do: {row, col}
   end
 
   def freqs(map) do
@@ -24,22 +19,39 @@ defmodule Day8 do
     |> Enum.group_by(&(at(map, &1)))
   end
 
+  def det([[a, b], [c, d]]), do: a * d - b * c
+
+  def contains({{x1, y1}, {x2, y2}}, {x3, y3}) do
+    mat = [[x2 - x1, y2 - y1], [x3 - x1, y3 - y1]]
+    det(mat) == 0
+  end
+
   def part1(map) do
     map
     |> freqs()
-    |> Enum.flat_map(fn {freq, indices} -> 
-      for {x1, y1} = p1 <- indices, {x2, y2} = p2 <- indices, p1 != p2 do 
-        index = {x1 - (x2 - x1), y1 - (y2 - y1)}
-        found = at(map, index)
-        if found == freq or found == nil, do: nil, else: index
+    |> Enum.flat_map(fn {freq, points} ->
+      for {x1, y1} = p1 <- points, {x2, y2} = p2 <- points, p1 != p2 do
+        {x1 - (x2 - x1), y1 - (y2 - y1)}
       end
+      |>Enum.reject(&(at(map, &1) in [nil, freq]))
     end)
-    |> Enum.reject(&(&1 == nil))
     |> Enum.uniq()
     |> Enum.count()
   end
 
-  def part2(_), do: "foo"
+  def part2(map) do
+    lines = 
+      map
+      |> freqs()
+      |> Enum.flat_map(fn {_, points} -> 
+        for p1 <- points, p2 <- points, p1 < p2, do: {p1, p2}
+      end)
+    
+    map
+    |> indices()
+    |> Enum.filter(fn point -> Enum.any?(lines, &contains(&1, point)) end)
+    |> Enum.count()
+  end
 end
 
 File.read!("./input.txt")
