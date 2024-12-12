ddbbbbce1b41da426a289292e9b0fc2c81ff92e0
Jochem Arends <77842807+jochemarends@users.noreply.github.com.>
2024-12-12 15:06:41 +0100

    working on day 12

--- 2024/day12.exs
+++ 2024/day12.exs
@@ -15,19 +15,33 @@ defmodule Day12 do
 
   defp adjacents({x, y}), do: [{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}]
 
-  defp is_interior?(garden, pos) do
-    adjacents(pos)
+  defp is_interior?(garden, plot) do
+    plot
+    |> adjacents()
     |> Enum.map(&Map.get(garden, &1))
     |> Enum.all?(&(&1 == Map.get(garden, pos)))
   end
 
-  defp group_regions(garden) do
-    Map.keys(garden)
-    |> Enum.reduce({[], %{}}, fn pos, {groups, visited} -> 
+  defp is_boundary?(garden, plot), do: not is_interior?(garden, plot)
 
-    end)
-    # find adjacent that's already in a list
-    # add it else add something else
+  defp borders_any?(region, plot), do: plot |> adjacents() |> Enum.any?(&(&1 in region))
+
+  defp group_type(garden) Enum.group_by(garden, fn plot -> Map.get(garden, plot) end)
+
+  defp walk_plots(plots, plot, visited \\ MapSet.new()) do
+    case Map.get(map, index) do
+      9 -> [index]
+      height ->
+        index
+        |> neighbours()
+        |> Enum.reject(&(&1 in visited))
+        |> Enum.filter(&(Map.get(map, &1) == height + 1))
+        |> Enum.flat_map(fn neighbour -> find_summits(map, neighbour, MapSet.put(visited, index)) end)
+    end
+  end
+
+  defp group_bordering([plot | plots]) do
+    # |> Enum.reduce([plot], fn plot, region, group_by(borders_any?
   end
 
   def part1(_), do: "todo"
